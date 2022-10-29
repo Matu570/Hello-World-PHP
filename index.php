d12f5502ab5dbdda8d05c7da51f0f6c0001583ea
Matias Bueno <matiasbueno@hotmail.com.ar>
2022-10-29 12:46:51 -0300

    Creating Hello World with PHP

---
+++ index.php
@@ -0,0 +1,7 @@
+<!-- For example, inside of PHP i can write HTML Tags-->
+<h1>Hello World, it is HTML</h1>
+<!--To can include PHP code, we must open and close the PHP tags, and put in there the code.-->
+<?php
+    // To print for console:
+    echo '<h2>Hello wolrd with PHP</h2>';
+?>
